b9e9683b53a5d4d65e9ca69290921393c7976c39
Mark Italia <markritalia@gmail.com>
2019-10-31 20:28:31 +0800

    Initial Release

---
+++ bris/src/com/italia/marxmind/bris/security/UserSession.java
@@ -0,0 +1,16 @@
+package com.italia.marxmind.bris.security;
+/**
+ * @deprecated 
+ * @author Mark Italia
+ * @version 1.0
+ * @since 10/23/2018
+ *
+ */
+
+public class UserSession {
+
+	public static void checkUserSession() {
+		
+	}
+	
+}
